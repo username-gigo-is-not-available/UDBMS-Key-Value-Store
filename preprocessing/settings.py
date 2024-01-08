da9a6fcea80a67e776ac7660a0f32326ea2d22b7
unknown <grigorij.josifovski@gmail.com>
2024-01-08 21:16:19 +0100

    Update environment variables

--- preprocessing/settings.py
+++ preprocessing/settings.py
@@ -4,7 +4,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-PATH_MEASUREMENT_SUMMARY_PATH = os.getenv("PATH_MEASUREMENT_SUMMARY")
-PATH_MEASUREMENT_ITEM_INFO_PATH = os.getenv("PATH_MEASUREMENT_ITEM_INFO")
-PATH_MEASUREMENT_INFO_PATH = os.getenv("PATH_MEASUREMENT_INFO")
-PATH_MEASUREMENT_STATION_INFO_PATH = os.getenv("PATH_MEASUREMENT_STATION_INFO")
+DATASET_PATH = os.getenv("DATASET_PATH")
+MEASUREMENT_INFO_PATH = "/".join([DATASET_PATH, "Measurement_info.csv"])
+MEASUREMENT_ITEM_INFO_PATH = "/".join([DATASET_PATH, "Measurement_item_info.csv"])
+MEASUREMENT_STATION_INFO_PATH = "/".join([DATASET_PATH, "Measurement_station_info.csv"])
+OUTPUT_PATH = "/".join([os.getenv("OUTPUT_PATH"), "output.csv"])
